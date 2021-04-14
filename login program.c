c01c9eb6e583d1719d09f3102eb708d7df9224c6
REYNIEL MARK REYES <76235804+Reyniel711@users.noreply.github.com>
2021-04-13 21:09:10 -0700

    Add files via upload

---
+++ login program.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int Id_Number, Pin;
+
+
+    printf("Enter your Id Number:\n");
+    scanf("%d",&Id_Number);
+    if(Id_Number == 0711);
+
+    printf("Enter your Pin:\n");
+    scanf("%d",&Pin);
+
+    if(Pin == 1234)
+{
+    printf("\n You have Successfully Logged In!");
+    printf ("\n Id_Number: 0711");
+}
+    else
+{
+    printf("\n Invalid ID/pin");
+}
+    return 0;
+}
